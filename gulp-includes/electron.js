cc6a38c754c2a5f445dc0108e37b2539104080f9
Nathan Kluth <nlkluth@gmail.com>
2015-09-03 10:53:48 -0600

    log error from electron

--- gulp-includes/electron.js
+++ gulp-includes/electron.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var run = require('gulp-run');
-var gutil = require('gulp-util');
 
 module.exports = function(gulp) {
   gulp.task('electron', ['scripts:dev'], function() {
-    return run('./node_modules/.bin/electron .').exec()
-      .on('error', gutil.log.bind(gutil, 'Electron Error'));
+    return run('./node_modules/.bin/electron .').exec(function(error) {
+      if (error) {
+        console.log(error);
+      }
+    });
   });
 };
