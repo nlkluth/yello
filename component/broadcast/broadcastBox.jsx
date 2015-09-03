9b32725d45d8f538ae43eefc7f3883a2597f4878
Nathan Kluth <nlkluth@gmail.com>
2015-09-03 12:00:52 -0600

    stub broadcast

---
+++ component/broadcast/broadcastBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class BroadcastBox extends React.Component {
+  render() {
+    return (
+      <div className="broadcastbox"> Broadcast </div>
+    )
+  }
+}
--- component/index.jsx
+++ component/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChatBox from './chat/chatBox.jsx';
 import ViewerBox from './viewers/viewerBox.jsx';
+import BroadcastBox from './broadcast/broadcastBox.jsx';
 import api from '../server/stubApi';
 
 class YelloApp extends React.Component {
@@ -21,6 +22,7 @@ class YelloApp extends React.Component {
     return (
       <div className="container">
         <ViewerBox viewers={this.state.viewers} open={this.state.open.viewers} />
+        <BroadcastBox broadcasters={this.state.broadcasters} open={this.state.open.broadcasters} />
         <ChatBox chat={this.state.chat} open={this.state.open.chat} />
       </div>
     )
