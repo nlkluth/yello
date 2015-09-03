5aa42911d3a6c45031e43dfa72d5f027f44a96ff
Nathan Kluth <nlkluth@gmail.com>
2015-09-03 10:52:35 -0600

    stubApi

---
+++ server/stubApi.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  chat: [{
+    text: 'test',
+    user: 'James',
+    id: 1
+  }, {
+    text: 'foo',
+    user: 'Fred',
+    id: 2
+  }],
+  broadcasters: [],
+  viewers: [],
+
+  getChat: function() {
+    return this.chat;
+  },
+
+  setChat: function(item) {
+    this.chat.push(item);
+  },
+
+  getBroadcasters: function() {
+    return this.broadcasters;
+  },
+
+  setBroadcasters: function(item) {
+    this.broadcasters.push(item);
+  },
+
+  getViewers: function() {
+    return this.viewers;
+  }
+};
