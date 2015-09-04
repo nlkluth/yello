bb70fbdb40af1b70e12180c18d813c327e364d52
Nathan Kluth <nlkluth@gmail.com>
2015-09-04 11:15:51 -0600

    setting up video

--- component/broadcast/broadcastView.jsx
+++ component/broadcast/broadcastView.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 export default class BroadcastView extends React.Component {
   render() {
     return (
-      <div className="broadcastView"> BroadcastView </div>
+      <div className="broadcastView container">
+        <video className="host" ref="hostScreen"></video>
+      </div>
     )
+  }
+
+  componentDidMount() {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    let constraints = {
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'screen',
+          maxWidth: screen.availWidth,
+          maxHeight: screen.availHeight,
+          maxFrameRate: 25
+        },
+        optional: []
+      }
+    };
+
+    navigator.getUserMedia(constraints, stream => {
+      let video = React.findDOMNode(this.refs.hostScreen);
+      video.src = window.URL.createObjectURL(stream);
+    }, e => console.log(e));
   }
 }
