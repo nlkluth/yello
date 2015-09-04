ef3f606b3b47cff2ced1ff72dc8c1332796544b2
Nathan Kluth <nlkluth@gmail.com>
2015-09-04 09:18:50 -0600

    changing lists around

--- component/chat/chat.jsx
+++ component/chat/chat.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Avatar from '../common/avatar.jsx';
 
 export default class Chat extends React.Component {
   render() {
     return (
       <li className="chat">
-        <Avatar />
-        <h2 className="chatAuthor">
-          {this.props.user}
-        </h2>
+        <span class="author">{this.props.user}</span>
         {this.props.children}
       </li>
     );
--- component/viewers/viewerList.jsx
+++ component/viewers/viewerList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Avatar from '../common/avatar.jsx';
 
 export default class ViewerList extends React.Component {
   render() {
     return (
-      <li>{this.props.data.user}</li>
+      <li>
+        <Avatar />
+        {this.props.data.user}
+      </li>
     )
   }
 }
