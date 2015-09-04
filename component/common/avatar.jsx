680f2f4bc95842110d9b734216848c6920c94587
Nathan Kluth <nlkluth@gmail.com>
2015-09-04 09:06:19 -0600

    adding avatar component

---
+++ component/common/avatar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Avatar extends React.Component {
+  render() {
+    return (
+      <img className="avatar" src="http://s1h.roomido.com/bundles/gujhomestylrfrontend/images/placeholder_avatar_300x300.png?20150903110023_34.6F" />
+    )
+  }
+}
