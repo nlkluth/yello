6689f1cddb34a46eb219dcaf325087a5dbe7a1f9
Nathan Kluth <nlkluth@gmail.com>
2015-09-08 10:24:49 -0600

    moving initial media load to helper

--- src/component/broadcast/broadcastView.jsx
+++ src/component/broadcast/broadcastView.jsx
@@ -16,46 +16,4 @@ export default class BroadcastView extends React.Component {
       </div>
     )
   }
-
-  componentDidMount() {
-    let videos = [];
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    let sourceSelected = (audioSource, videoSource) => {
-      let constraints = {
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: 'screen',
-            maxWidth: screen.availWidth,
-            maxHeight: screen.availHeight,
-            maxFrameRate: 25
-          },
-          optional: [{sourceId: videoSource}]
-        }
-      };
-
-      navigator.getUserMedia(constraints, stream => {
-        dispatch(broadcasters.newUser(stream));
-      }, e => console.log(e));
-    };
-
-    MediaStreamTrack.getSources(sourceInfos => {
-      var audioSource = null;
-      var videoSource = null;
-
-      sourceInfos.forEach(info => {
-        if (info.kind === 'audio') {
-          audioSource = info.id;
-        }
-
-        if (info.kind === 'video') {
-          videoSource = info.id;
-        }
-      })
-
-      sourceSelected(audioSource, videoSource);
-    });
-  }
 }
